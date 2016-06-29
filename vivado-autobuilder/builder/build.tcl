4e6dffe3f6cfc188e302269292931a14fda4bc61
Daichi Teruya <tmarusa83@gmail.com>
2016-06-29 02:43:34 -0400

    implement builder

--- vivado-autobuilder/builder/build.tcl
+++ vivado-autobuilder/builder/build.tcl
@@ -8,6 +8,15 @@ create_project -force $project_name $project_directory
 
 source builder/project_generator.tcl
 
+launch_runs synth_1 -job 8
+wait_on_run synth_1
+
+launch_runs impl_1 -job 8
+wait_on_run impl_1
+
+launch_runs impl_1 -to_step write_bitstream -job 8
+wait_on_run impl_1
+
 close_project
 
 
